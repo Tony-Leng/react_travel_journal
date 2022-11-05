58b425e8f827b480c55b35d7d46b7c95513985c6
Tony Leng <lengtk@gmail.com>
2022-11-05 22:09:21 +0900

    started navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,8 @@ import React from 'react'
 export default function Navbar() {
   return (
     <nav>
-      <h3>my travel journal</h3>
+      <img className='japanicon' src='images/japan_icon.png' alt='icon of japan'></img>
+      <p>my travel journal</p>
     </nav>
   )
 }

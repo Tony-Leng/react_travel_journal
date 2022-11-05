df4a10236c59fac5f1e58011c0e5683b82a025a5
Tony Leng <lengtk@gmail.com>
2022-11-05 21:27:47 +0900

    setup components, linked html to js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+import React from 'react'
+// import logo from './logo.svg';
+// import Card from './components/Card';
+import Navbar from './components/Navbar';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      {/* <Card /> */}
     </div>
   );
 }
-
-export default App;

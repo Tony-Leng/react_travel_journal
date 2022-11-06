60b066a6ed4a9f5d68d65addd53f4048e3512c26
Tony Leng <lengtk@gmail.com>
2022-11-06 17:19:17 +0900

    updated all locations in data.js file

--- src/data.js
+++ src/data.js
@@ -1,16 +1,7 @@
 export default [
   {
-      title: "Mount Fuji",
-      location: "Japan",
-      googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-      startDate: "12 Jan, 2021",
-      endDate: "24 Jan, 2021",
-      description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-      imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
-  },
-  {
     title: "Mount Fuji",
-    location: "Japan",
+    location: "Yamanashi",
     googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
     startDate: "12 Jan, 2021",
     endDate: "24 Jan, 2021",
@@ -18,12 +9,21 @@ export default [
     imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
   },
   {
-    title: "Mount Fuji",
-    location: "Japan",
-    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-    startDate: "12 Jan, 2021",
-    endDate: "24 Jan, 2021",
-    description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-    imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+    title: "Arashiyama Bamboo Forest",
+    location: "Kyoto",
+    googleMapsUrl: "https://goo.gl/maps/abFsecYe2X5krtgP9",
+    startDate: "12 Feb, 2021",
+    endDate: "24 Feb, 2021",
+    description: "Arashiyama is a district on the western outskirts of Kyoto, Japan. It is a nationally designated Historic Site and Place of Scenic Beauty.",
+    imageUrl: "https://unsplash.com/photos/041DmlRyS3k"
+  },
+  {
+    title: "Nara Deer Park",
+    location: "Nara",
+    googleMapsUrl: "https://goo.gl/maps/1rZad8ubtxoyY4A5A",
+    startDate: "12 Mar, 2021",
+    endDate: "24 Mar, 2021",
+    description: "Nara Park is a public park located in the city of Nara, Japan, at the foot of Mount Wakakusa. Established in 1880, it is one of the oldest parks in Japan.",
+    imageUrl: "https://unsplash.com/photos/Y44GUJey18M"
   },
 ]

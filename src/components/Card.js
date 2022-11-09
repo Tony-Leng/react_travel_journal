9e68c2e49eb98ce382d6fee3ead70310140ced1f
Tony Leng <lengtk@gmail.com>
2022-11-09 14:32:44 +0900

    refactored to spread object as prop

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,15 +3,15 @@ import React from 'react'
 export default function Card(props) {
   return (
     <div className='card'>
-      <img src={props.item.imageUrl} alt="location image" className='card-image'></img>
+      <img src={props.imageUrl} alt="location image" className='card-image'></img>
       <div className='card-description'>
         <div className='card-location'>
-          <h3>{props.item.location}</h3>
-          <a href={props.item.googleMapsUrl} target="_blank">View on Google Maps</a>
+          <h3>{props.location}</h3>
+          <a href={props.googleMapsUrl} target="_blank">View on Google Maps</a>
         </div>
-        <h1 className='card-title'>{props.item.title}</h1>
-        <h4 className='card-dates'>{props.item.startDate} - {props.item.endDate}</h4>
-        <p>{props.item.description}</p>
+        <h1 className='card-title'>{props.title}</h1>
+        <h4 className='card-dates'>{props.startDate} - {props.endDate}</h4>
+        <p>{props.description}</p>
       </div>
     </div>
   )
